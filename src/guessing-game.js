eb18f380d10e24103b459cc36e55dd6cd8623b78
HAROSHII232 <igorgrime@gmail.com>
2023-09-16 11:49:24 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.middle = Math.round((this.max - this.min) / 2 + this.min)
     }
 
     guess() {
-
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
+        this.middle = Math.round((this.max - this.min) / 2 + this.min);
     }
 
     greater() {
-
+        this.min = this.middle;
+        this.middle = Math.round((this.max - this.min) / 2 + this.min);
     }
 }
 
